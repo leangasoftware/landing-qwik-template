1e24a136ed889a2ed303ca9e84e927f5101604e1
Leifer Mendez <leifer33@gmail.com>
2022-09-22 13:36:25 +0200

    ready

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,11 @@
-import { defineConfig } from "vite";
-import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import netlifyEdge from "@netlify/vite-plugin-netlify-edge";
 
 export default defineConfig(() => {
   return {
-    plugins: [
-      qwikCity(),
-      qwikVite({
-        ssr: {
-          outDir: "netlify/edge-functions",
-        },
-      }),
-      tsconfigPaths(),
-      netlifyEdge({
-        functionName: "entry.netlify",
-      }),
-    ],
+    ssr: { target: "webworker", noExternal: true },
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
   };
 });
