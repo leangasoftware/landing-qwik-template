b07eef1f691957a192b7bab59c2b116ec35e86e6
Leifer Mendez <leifer33@gmail.com>
2022-09-23 21:09:42 +0200

    google g4

--- src/root.tsx
+++ src/root.tsx
@@ -5,15 +5,7 @@ import Head from "./components/head/head";
 import "./global.css";
 
 export default () => {
-  const snippetText = partytownSnippet({
-    resolveUrl: function (url) {
-      const proxyMap: any = {
-        "www.googletagmanager.com": "d2lderwexwm7la.cloudfront.net",
-      };
-      url.hostname = proxyMap[url.hostname] || url.hostname;
-      return url;
-    },
-  });
+  const snippetText = partytownSnippet();
   const googlePixel = `function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-NHJQPYGYCB");`;
   const facebookPixel = `!function(e,t,n,a,c,o,s){e.fbq||(c=e.fbq=function(){c.callMethod?c.callMethod.apply(c,arguments):c.queue.push(arguments)},e._fbq||(e._fbq=c),c.push=c,c.loaded=!0,c.version="2.0",c.queue=[],(o=t.createElement(n)).async=!0,o.src=a,(s=t.getElementsByTagName(n)[0]).parentNode.insertBefore(o,s))}(window,document,"script","https://connect.facebook.net/en_US/fbevents.js"),fbq("init","1236522833796620"),fbq("track","PageView");`;
 
