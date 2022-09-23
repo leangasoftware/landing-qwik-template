50e2cfba2b04a2743d8f206228ddd00402e09c07
Leifer Mendez <leifer33@gmail.com>
2022-09-23 21:51:12 +0200

    fb pixel

--- src/root.tsx
+++ src/root.tsx
@@ -5,7 +5,20 @@ import Head from "./components/head/head";
 import "./global.css";
 
 export default () => {
-  const snippetText = partytownSnippet();
+  const snippetText = partytownSnippet({
+    resolveUrl: function (url) {
+      if (url.hostname === "connect.facebook.net") {
+        const proxyMap: any = {
+          "connect.facebook.net": "d2gn02yz3528v5.cloudfront.net",
+        };
+        url.hostname = proxyMap[url.hostname] || url.hostname;
+        return url;
+      }
+      return url;
+    },
+    forward: ["fbq"],
+  });
+
   const googlePixel = `function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-NHJQPYGYCB");`;
   const facebookPixel = `!function(e,t,n,a,c,o,s){e.fbq||(c=e.fbq=function(){c.callMethod?c.callMethod.apply(c,arguments):c.queue.push(arguments)},e._fbq||(e._fbq=c),c.push=c,c.loaded=!0,c.version="2.0",c.queue=[],(o=t.createElement(n)).async=!0,o.src=a,(s=t.getElementsByTagName(n)[0]).parentNode.insertBefore(o,s))}(window,document,"script","https://connect.facebook.net/en_US/fbevents.js"),fbq("init","1236522833796620"),fbq("track","PageView");`;
 
